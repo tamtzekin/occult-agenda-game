f0db0dd6e883d4798fd453478bcdd87b393d7220
jae <jsmchugh@gmail.com>
2018-12-06 09:50:46 +1100

    Twitter usernames link to webpages for twitter accounts

---
+++ Assets/_Assets/Scripts/CreditLink.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditLink : MonoBehaviour
+{
+	[SerializeField]
+	private string websiteLink;
+
+	public void OpenLinkInBrowser()
+	{
+		Application.OpenURL(websiteLink);
+	}
+}
