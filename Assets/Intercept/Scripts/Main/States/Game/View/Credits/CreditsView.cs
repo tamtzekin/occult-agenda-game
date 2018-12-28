850ba511ffcfd208c5721be680368950bacfaf1c
jae <jsmchugh@gmail.com>
2018-12-28 11:27:04 +1100

    Credits quit button

--- Assets/Intercept/Scripts/Main/States/Game/View/Credits/CreditsView.cs
+++ Assets/Intercept/Scripts/Main/States/Game/View/Credits/CreditsView.cs
@@ -6,12 +6,15 @@ public class CreditsView : MonoBehaviour {
 	public void Show () {
 		gameObject.SetActive(true);
 		transform.Find("Restart Button").gameObject.SetActive(false);
+		transform.Find("Quit Button").gameObject.SetActive(false);
 	}
 
 	public void ShowEndCredits()
 	{
 		gameObject.SetActive(true);
 		transform.Find("Back Button").gameObject.SetActive(false);
+		transform.Find("Restart Button").gameObject.SetActive(true);
+		transform.Find("Quit Button").gameObject.SetActive(true);
 	}
 
 	public void Hide () {
