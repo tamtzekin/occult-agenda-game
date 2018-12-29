cbcda0339f514c4879295e841d0953802cb0f5f9
jae <jsmchugh@gmail.com>
2018-12-29 19:08:30 +1100

    Sanity page glow fade in

--- Assets/_Assets/Scripts/AddGlowToPage.cs
+++ Assets/_Assets/Scripts/AddGlowToPage.cs
@@ -6,9 +6,30 @@ public class AddGlowToPage : MonoBehaviour
 {
 	[SerializeField] Material glowMaterial;
 
+	private FloatTween glowTween = new FloatTween();
+
 	public void AddGlow()
 	{
+		glowTween.OnChange += ChangeGlowTween;
+		StartCoroutine(GlowIn(2f));
+		
+	}
+
+	IEnumerator GlowIn(float fadeTime)
+	{
 		Main.Instance.gameState.contentManager.paperImage.material = new Material(glowMaterial);
+		glowTween.Tween(2.2f, 1.2f, fadeTime);
+		while (glowTween.tweening)
+		{
+			if (glowTween.tweening)
+				glowTween.Loop();
+			yield return null;
+		}
+	}
+
+	void ChangeGlowTween(float currentValue)
+	{
+		Main.Instance.gameState.contentManager.paperImage.material.SetFloat("_OutlineSize", currentValue);
 	}
 
 	public void RemoveGlow()
