5e5c7eb1236032a0f06c3326ed2a78b7fcfb0aa7
jae <jsmchugh@gmail.com>
2018-12-29 21:53:09 +1100

    Sanity page glow removal fades out

--- Assets/_Assets/Scripts/AddGlowToPage.cs
+++ Assets/_Assets/Scripts/AddGlowToPage.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class AddGlowToPage : MonoBehaviour
 {
 	[SerializeField] Material glowMaterial;
 
 	private FloatTween glowTween = new FloatTween();
+	private FloatTween bloomTween = new FloatTween();
+
+	[SerializeField] PostProcessVolume postProcessVolume;
+
+	Bloom bloom;
+
+	float startIntensity;
+
+	void Start()
+	{
+		PostProcessProfile profile = postProcessVolume.profile;
+		bloom = profile.GetSetting<Bloom>();
+		startIntensity = bloom.intensity.value;
+		bloomTween.OnChange += ChangeBloomTween;
+
+		glowTween.OnChange += ChangeGlowTween;
+	}
 
 	public void AddGlow()
 	{
-		glowTween.OnChange += ChangeGlowTween;
 		StartCoroutine(GlowIn(2f));
 		
 	}
@@ -34,6 +51,26 @@ public class AddGlowToPage : MonoBehaviour
 
 	public void RemoveGlow()
 	{
+		// fade bloom intensity down then remove
+		// not down to zero or the effect isn't there - maybe 0.4?
+		StartCoroutine(BloomDown(2f));
+	}
+
+	IEnumerator BloomDown(float fadeTime)
+	{
+		bloomTween.Tween(0f, 1.0f, fadeTime);
+		while (bloomTween.tweening)
+		{
+			if (bloomTween.tweening)
+				bloomTween.Loop();
+			yield return null;
+		}
 		Main.Instance.gameState.contentManager.paperImage.material = null;
+		bloom.intensity.value = startIntensity;
+	}
+
+	void ChangeBloomTween(float currentValue)
+	{
+		bloom.intensity.Interp(startIntensity, 0.4f, currentValue);
 	}
 }
